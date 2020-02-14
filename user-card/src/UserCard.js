0ef6a7ede1d33cc75705f87f1d53e4bdd2fd512a
adella <scratchglory@gmail.com>
2020-02-13 20:52:17 -0500

    added styles to cards

--- user-card/src/UserCard.js
+++ user-card/src/UserCard.js
@@ -14,7 +14,7 @@ const UserCard = props => {
       <h2>The Followers</h2>
       {props.followers.map(per => {
         return (
-          <div className="user-card">
+          <div className="follower-card">
             <Image src={per.avatar_url} />
             <h4>{per.login}</h4>
           </div>
